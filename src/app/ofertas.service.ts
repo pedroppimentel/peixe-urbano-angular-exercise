7b883d350c77b8009b2981037cc4396816681aa9
Pedro P Pimentel <pedrop.pimentel@hotmail.com>
2020-06-28 14:00:49 -0300

    Start binding oferta

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -14,4 +14,12 @@ export class OfertasService {
     getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
         return this.http.get<Oferta[]>(`http://localhost:3000/ofertas?categoria=${ categoria }`).toPromise();
     }
+
+    getOfertaById(id: number): Promise<Oferta> {
+        return this.http.get(`http://localhost:3000/ofertas?id=${ id }`)
+        .toPromise()
+        .then((resposta: any) => {
+            return resposta[0];
+        })
+    }
 }
